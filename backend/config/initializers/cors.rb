243e6914a068f2ae0076a4f6d0c6c53959c20467
Odu-Enkay <[erozonankiru@gmail.com]>
2025-10-20 16:45:08 -0600

    update cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,8 +1,20 @@
+#Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#  allow do
+ #   origins 'https://buybuy-p8wt.onrender.com', 'https://buybuy-frontend.onrender.com'
+  #  resource '*',
+   #   headers: :any,
+    #  methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  #end
+#end
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://buybuy-p8wt.onrender.com', 'https://buybuy-frontend.onrender.com'
+    origins 'https://buybuy-frontend.onrender.com' # frontend URL
+
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization']
   end
 end
+
